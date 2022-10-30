06781787c2f1c550c3d4191772054fadf03a495d
robroskie <lroblesky@gmail.com>
2022-10-30 13:19:41 -0700

    bot able to receive and respond to messages

---
+++ corgi-chatbot/Modules/Commands.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Discord.Commands;
+
+namespace corgi_chatbot.Modules
+{
+    public class Commands : ModuleBase<SocketCommandContext>
+    {
+        [Command("ping")]
+        public async Task Ping()
+        {
+            await ReplyAsync("Hai");
+        }
+    }
+}
+
--- corgi-chatbot/Program.cs
+++ corgi-chatbot/Program.cs
@@ -1,44 +1,121 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Configuration;
+using System;
+using System.Reflection;
+using System.Threading;
 using System.Threading.Tasks;
-
+using Microsoft.Extensions.DependencyInjection;
+using corgi.Services;
 using Discord;
 using Discord.Commands;
 using Discord.WebSocket;
 
-using Microsoft.Extensions.DependencyInjection;
+using Discord.Interactions;
 
-using System.Configuration;
-using System.Collections.Specialized;
-
-namespace HelloWorld
+public class Program
 {
-    class Program
+    public static Task Main(string[] args) => new Program().MainAsync();
+
+    private DiscordSocketClient _client;
+    private InteractionService _commands;
+    private CommandHandler _handler;
+
+    public async Task MainAsync()
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-            string sAttr = ConfigurationManager.AppSettings.Get("Setting1");
-            Console.WriteLine(sAttr);
-        }
+    
+        
+            string botToken = ConfigurationManager.AppSettings.Get("Key1");
 
-        private DiscordSocketClient _client;
-        private CommandService _commands;
-        private IServiceProvider _services;
 
-        public async Task RunBotAsync()
-        {
             _client = new DiscordSocketClient();
-            _commands = new CommandService();
+            _client.Log += Log;
 
-            _services = new ServiceCollection()
-                .AddSingleton(_client)
-                .AddSingleton(_commands)
-                .BuildServiceProvider();
 
-         
+            await _client.LoginAsync(TokenType.Bot, botToken);
+            await _client.StartAsync();
+
+            _client.MessageReceived += MessageReceived;
+
+
+
+            // Block this task until the program is closed.
+            await Task.Delay(-1);
+        
+
+    }
+
+    private async Task MessageReceived(SocketMessage message)
+    {
+        if (!message.Author.IsBot)
+        {
+            await message.Channel.SendMessageAsync("Hello!");
         }
     }
+
+
+
+        private Task Log(LogMessage msg)
+    {
+        Console.WriteLine(msg.ToString());
+        return Task.CompletedTask;
+    }
 }
+
+
+public class CommandHandler
+{
+    private readonly DiscordSocketClient _client;
+    private readonly CommandService _commands;
+
+    // Retrieve client and CommandService instance via ctor
+
+
+    public async Task InstallCommandsAsync()
+    {
+        // Hook the MessageReceived event into our command handler
+        _client.MessageReceived += HandleCommandAsync;
+
+        // Here we discover all of the command modules in the entry 
+        // assembly and load them. Starting from Discord.NET 2.0, a
+        // service provider is required to be passed into the
+        // module registration method to inject the 
+        // required dependencies.
+        //
+        // If you do not use Dependency Injection, pass null.
+        // See Dependency Injection guide for more information.
+        await _commands.AddModulesAsync(assembly: Assembly.GetEntryAssembly(),
+                                        services: null);
+    }
+
+
+
+    private async Task HandleCommandAsync(SocketMessage messageParam)
+    {
+        // Don't process the command if it was a system message
+        var message = messageParam as SocketUserMessage;
+        if (message == null) return;
+
+        // Create a number to track where the prefix ends and the command begins
+        int argPos = 0;
+
+        // Determine if the message is a command based on the prefix and make sure no bots trigger commands
+        if (!(message.HasCharPrefix('!', ref argPos) ||
+            message.HasMentionPrefix(_client.CurrentUser, ref argPos)) ||
+            message.Author.IsBot)
+            return;
+
+        // Create a WebSocket-based command context based on the message
+        var context = new SocketCommandContext(_client, message);
+
+        // Execute the command with the command context we just
+        // created, along with the service provider for precondition checks.
+        await _commands.ExecuteAsync(
+            context: context,
+            argPos: argPos,
+            services: null);
+    }
+}
+
+
+
+
+
